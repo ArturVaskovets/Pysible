536ae23f8bab0a7bfe8c9c83a8a74fcfa425f369
ArturVaskovets <artik2607@gmail.com>
2020-06-04 15:47:08 +0200

    RequiredIf validator added Test files separated from main project

--- forms.py
+++ forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm # pylint: disable=import-error
 from wtforms import RadioField, BooleanField,StringField, SelectField, PasswordField, TextField # pylint: disable=import-error
 from wtforms.fields.html5 import EmailField # pylint: disable=import-error
-from wtforms.validators import Required, Length # pylint: disable=import-error
+from wtforms.validators import Required, Length, InputRequired, Optional # pylint: disable=import-error
+
+class RequiredIf(InputRequired):
+    """Validator which makes a field required if another field is set and has a truthy value."""
+    field_flags = ('requiredif',)
+
+    def __init__(self, other_field_name, message=None, *args, **kwargs):
+        self.other_field_name = other_field_name
+        self.message = message
+
+    def __call__(self, form, field):
+        other_field = form[self.other_field_name]
+        if other_field is None:
+            raise Exception('no field named "%s" in form' % self.other_field_name)
+        if bool(other_field.data):
+            super(RequiredIf, self).__call__(form, field)
+        else:
+            Optional().__call__(form, field)
 
 class MainForm(FlaskForm):
+	"""The form that contains all the options to generate a playbook"""
 	#General
 	playbook_name = StringField('Playbook name',default='playbook', validators=[Required('This field is required')])
 	description = TextField('Description',default='')
@@ -12,7 +30,7 @@ class MainForm(FlaskForm):
 	#Webservers section
 	install_webserver = BooleanField('Install webserver', default=False)
 	server_types = RadioField('Webserver type', choices=[('apache','Apache2'),('nginx','Nginx')], default='apache')
-	document_root = StringField('Document root',default='/var/www/html')
+	document_root = StringField('Document root', validators=[RequiredIf('install_webserver')], default='/var/www/html')
 	server_name = StringField('Server name',default='')
 
 
@@ -28,12 +46,12 @@ class MainForm(FlaskForm):
 
 	#Databases
 	install_db = BooleanField('Install database', default=False)
-	db_types = RadioField('Database type', choices=[('mysql','MySQL'),('postgresql','PostgreSQL'),('sqlite','SQLite')], default='mysql')
+	db_types = RadioField('Database management system', choices=[('mysql','MySQL'),('postgresql','PostgreSQL'),('sqlite','SQLite')], default='mysql')
 	db_delete_anonymous = BooleanField('Delete anonymous user', default=False)
 	db_delete_test = BooleanField('Delete test database', default=False)
 	db_create_user = BooleanField('Create user', default=False)
-	db_username = StringField('Username', validators=[Length(message=u'Username length must be in the range 3-20 characters', min=3, max=20)], default='user')
-	db_password = StringField('Password', validators=[Length(message=u'Password length must be in the range 6-20 characters', min=6, max=20)], default='password')
+	db_username = StringField('Username', validators=[RequiredIf('db_create_user'), Length(message=u'Username length must be in the range 3-20 characters', min=3, max=20)])
+	db_password = StringField('Password', validators=[RequiredIf('db_create_user'), Length(message=u'Password length must be in the range 6-20 characters', min=6, max=20)])
 
 
 class LoginForm(FlaskForm):
