090878ad1754b4ad4a0acbda080a61f73deeedb1
ArturVaskovets <artik2607@gmail.com>
2020-06-10 18:55:34 +0200

    NoneType fix

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ DEBUG = False
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
 db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_connection_name = '/cloudsql/' + os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+db_connection_name = '/cloudsql/' + str(os.environ.get('CLOUD_SQL_CONNECTION_NAME'))
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}?unix_socket={}'.format(db_user, db_password, db_name, db_connection_name)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
