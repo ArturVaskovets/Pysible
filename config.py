052f3463191cd8b6fa7b6b1c6d55e801fd97385e
ArturVaskovets <artik2607@gmail.com>
2020-06-10 19:17:26 +0200

    Fix

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from Pysible.main import app
 SECRET_KEY = b'w\x17\xf10\x1a\xde\xab~\xb9\xd2\x04\xe5\xc0\x81\xd8\xdc'
 PWD = os.path.normpath(app.root_path)
 PROJECTS_DIR = os.path.join(PWD, 'user_projects/')
-APP_UID = os.stat(os.path.join(PWD, 'app.py')).st_uid # Get application owners uid
-APP_GID = os.stat(os.path.join(PWD, 'app.py')).st_gid # Get application owners uid
+APP_UID = os.stat(os.path.join(PWD, 'main.py')).st_uid # Get application owners uid
+APP_GID = os.stat(os.path.join(PWD, 'main.py')).st_gid # Get application owners uid
 
 DEBUG = False
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
